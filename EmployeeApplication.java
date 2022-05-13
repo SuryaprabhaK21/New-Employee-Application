c47d947834ece04e14f6d1efbb7f1a037c673504
SuryaprabhaK21 <83077722+SuryaprabhaK21@users.noreply.github.com>
2022-05-13 16:57:50 +0530

    Add files via upload

---
+++ EmployeeApplication.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class EmployeeApplication
+{
+	public static void main(String[] args) 
+	{
+		//Employee employee = new Employee(null,null,null,null,null,null);
+		//EmployeeDatas employeeDatas=new EmployeeDatas();
+		Employee employee = new Employee();
+		employee.employeeDetails();
+  
+		
+	}
+
+}
